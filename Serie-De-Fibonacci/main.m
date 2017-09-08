37cd1f1fbdb78b563f25f24b4da039bbde596418
Ivan Ortega Victoriano <iortegav1500@alumno.ipn.mx>
2017-09-07 20:25:29 -0500

    Ya guarda los numeros a un archivo

--- Serie-De-Fibonacci/main.m
+++ Serie-De-Fibonacci/main.m
@@ -1,11 +1,15 @@
 clc % Limpiamos la ventana de comandos
 clear % Eliminamos los items del workspace
-limite = inputdlg('Ingresa el limite para la serie de Fibonacci:');
+resultados = fopen('numerosDeFibonacci.txt','w');
+limite = inputdlg('Ingresa el limite para la serie de Fibonacci:'); % Archivo para guardar los terminos
 n = str2double(limite{:}); % Convertimos a cadena a numero
 A = zeros(1,n+1);   % Arreglo para guardar los valores de la serie
+fprintf(resultados,'Primeros %d términos de la Serie de Fibonacci:\n',n);
 for i=1:n+1
     A(:,i) = fibonacci(i-1); % Guardamos los valores calculados en un array
+    fprintf(resultados,'%d\n',A(:,i));
 end
+fclose(resultados);
 figure % Se plotean los valores
 rango = 0:1:n;
 stem(rango,A);
