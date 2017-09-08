5ced9881d7c187b94dc37807431c13a56569b974
Ivan Ortega Victoriano <iortegav1500@alumno.ipn.mx>
2017-09-07 21:49:59 -0500

    Ya guarda los valores a un archivo

--- Serie-De-Fibonacci/main.m
+++ Serie-De-Fibonacci/main.m
@@ -1,3 +1,4 @@
+
 clc % Limpiamos la ventana de comandos
 clear % Eliminamos los items del workspace
 resultados = fopen('numerosDeFibonacci.txt','w');
@@ -7,12 +8,12 @@ A = zeros(1,n+1);   % Arreglo para guardar los valores de la serie
 fprintf(resultados,'Primeros %d términos de la Serie de Fibonacci:\n',n);
 for i=1:n+1
     A(:,i) = fibonacci(i-1); % Guardamos los valores calculados en un array
-    fprintf(resultados,'%d\n',A(:,i));
+    fprintf(resultados,'%d\n',A(:,i)); % Imprimimos los valores al archivo
 end
-fclose(resultados);
+fclose(resultados); % Cerramos el archivo
 figure % Se plotean los valores
 rango = 0:1:n;
-stem(rango,A);
+stem(rango,A);  % Ploteo de valores discretos
 xlabel('n')
 ylabel('Fibonacci(n)')
 titulo = strcat('Serie de Fibonacci de 0 a',{' '},num2str(n));
