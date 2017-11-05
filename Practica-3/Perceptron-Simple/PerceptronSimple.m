2256b430e621087cb491cad1f267552e051de001
Ivan Ortega Victoriano <iortegav1500@alumno.ipn.mx>
2017-11-04 18:06:38 -0600

    Aun no esta el método gráfico

--- Practica-3/Perceptron-Simple/PerceptronSimple.m
+++ Practica-3/Perceptron-Simple/PerceptronSimple.m
@@ -1,39 +1,168 @@
-clc 
+clc
 clear
-disp('PERCEPTR覰 SIMPLE');
-tipo = input('1)M閠odo gr醘ico\n2)Regla de aprendizaje\nIngrese su elecci髇:  ','s');
+disp('PERCEPTR脫N SIMPLE');
+tipo = input('1)M茅todo gr谩fico\n2)Regla de aprendizaje\nIngrese su elecci贸n:  ','s');
 switch(tipo)
     case '1'
         disp('MG');
-        numProt = input('Ingresa el n鷐ero de patrones prototipo:  ');
-        res = input('縇a dimensi髇 es menor o igual a 3? (s/n):  ','s');
-        protDim = input('Ingresa la dimensi髇 de los vectores prototipo:  ');
+        numProt = input('Ingresa el n煤mero de patrones prototipo:  ');
+        res = input('驴La dimensi贸n es menor o igual a 3? (s/n):  ','s');
+        R = input('Ingresa la dimensi贸n de los vectores prototipo:  ');
+        while R > 4 && R < 1
+            R = input('Ingresa una dimensi贸n adecuada:  ');
+        end
         if strcmp(res,'s') || strcmp(res,'S')
-            res = input('縇a dimensi髇 de los targets es mayor a 1?','s');
+            res = input('驴La dimensi贸n de los targets es mayor a 1? (s/n):  ','s');
             if strcmp(res,'s') || strcmp(res,'S')
-                targetDim = input('Ingresa la dimensi髇 de los targets:  ');
+                targetDim = input('Ingresa la dimensi贸n de los targets:  ');
                 disp('Ingresa el nombre del archivo que contiene el Cto. de Entrenamiento\n');
-                disp('(Sin extensi髇)');
+                disp('(Sin extensi贸n)');
                 nombre = input('Nombre del Archivo:  ','s');
                 nombre = strcat(nombre,'.txt');
-                prototipos = leerPatronesPrototipo(nombre,targetDim,numProt,protDim);
-                targets = leerTargets(nombre,targetDim,numProt,protDim);
+                prototipos = leerPatronesPrototipo(nombre,targetDim,numProt,R);
+                targets = leerTargets(nombre,targetDim,numProt,R);
                 disp('Prototipos =');
                 disp(prototipos);
                 disp('Targets =');
                 disp(targets);
+                numeroDeClases = input('Ingresa el n煤mero de clases:  ');
+                fprintf('Numero de clases: %d\n',numeroDeClases);
             else
                 disp('Ingresa el nombre del archivo que contiene el Cto. de Entrenamiento\n');
-                disp('(Sin extensi髇)');
+                disp('(Sin extensi贸n)');
                 nombre = input('Nombre del Archivo:  ','s');
-                nombre = strcar(nombre,'.txt');
+                nombre = strcat(nombre,'.txt');
                 ctoDeEntrenamiento = dlmread(nombre);
+                disp(ctoDeEntrenamiento);
+                dimensiones = size(ctoDeEntrenamiento);
+                R = dimensiones(2)-1;
+                prototipos = ctoDeEntrenamiento(:,1:R);
+                targets = ctoDeEntrenamiento(:,R-1:R);
+                disp(prototipos);
+                disp(targets);
+                numeroDeClases = input('Ingresa el n煤mero de clases:  ');
+                n = 1;
+                S = 1;
+                while numeroDeClases > pow(2,n)
+                    n = n+1;
+                    S = S+1;
+                end
+                
             end
         else
+            disp('El m茅todo gr谩fico solo funciona para vecotres prototipo de dimensi贸n menor o igual a 3.');
             exit(0);
         end
     case '2'
         disp('RDA');
-    default
-        display('Opci髇 inv醠ida');
+        numProt = input('Ingresa el n煤mero de patrones prototipo:  ');
+        R = input('Ingresa la dimensi贸n de los vectores prototipo:  ');
+        res = input('驴La dimensi贸n de los targets es mayor a 1? (s/n):  ','s');
+        if strcmp(res,'s') || strcmp(res,'S')
+            targetDim = input('Ingresa la dimensi贸n de los targets:  ');
+            disp('Ingresa el nombre del archivo que contiene el Cto. de Entrenamiento\n');
+            disp('(Sin extensi贸n)');
+            nombre = input('Nombre del Archivo:  ','s');
+            nombre = strcat(nombre,'.txt');
+            prototipos = leerPatronesPrototipo(nombre,targetDim,numProt,R);
+            targets = leerTargets(nombre,targetDim,numProt,R);
+            disp('Prototipos =');
+            disp(prototipos);
+            disp('Targets =');
+            disp(targets);
+            numeroDeClases = input('Ingresa el n煤mero de clases:  ');
+            n = 1;
+            S = 1.0;
+            while numeroDeClases > power(2,n)
+                n = n+1;
+                S = S+1.0;
+            end
+            itmax = input('Itmax:  ');
+            Eit = input('Eit:  ');
+            W = ones(S,R)*(mod(rand(),2));
+            b = ones(S,1)*(mod(rand(),2));
+            disp('Se usar谩n las siguientes matrices iniciales:');
+            disp('W =');
+            disp(W);
+            disp('b =');
+            disp(b);
+            for i=1:itmax
+                eit = 0;
+                t = 0;
+                for j=1:numProt
+                    a = hardlim(W*prototipos(j,:)'+b);
+                    disp(a);
+                    ej = targets(j,:)'-a;
+                    disp(ej);
+                    if ej ~= 0
+                        t = t+1;
+                        W = W+ej*prototipos(j,:);
+                        b = b+ej;
+                        eit = eit+(1/numProt)*ej;
+                    end
+                end
+                if isequal(eit,Eit) && t == 0
+                    fprintf('Se tuvo un aprendizaje exitoso en la iteraci贸n %d.\n',i);
+                    break;
+                end
+            end
+            disp('W =');
+            disp(W);
+            disp('b =');
+            disp(b);
+        else
+            disp('Ingresa el nombre del archivo que contiene el Cto. de Entrenamiento\n');
+            disp('(Sin extensi贸n)');
+            nombre = input('Nombre del Archivo:  ','s');
+            nombre = strcat(nombre,'.txt');
+            ctoDeEntrenamiento = dlmread(nombre);
+            dimensiones = size(ctoDeEntrenamiento);
+            R = dimensiones(2)-1;
+            prototipos = ctoDeEntrenamiento(:,1:R);
+            targets = ctoDeEntrenamiento(:,R+1);
+            disp('Prototipos =');
+            disp(prototipos);
+            disp('Targets =');
+            disp(targets);
+            numeroDeClases = input('Ingresa el n煤mero de clases:  ');
+            n = 1;
+            S = 1.0;
+            while numeroDeClases > power(2,n)
+                n = n+1;
+                S = S+1.0;
+            end
+            itmax = input('Itmax:  ');
+            Eit = input('Eit:  ');
+            W = ones(S,R)*(mod(rand(),2));
+            b = ones(S,1)*(mod(rand(),2));
+            disp('Se usar谩n las siguientes matrices iniciales:');
+            disp('W =');
+            disp(W);
+            disp('b =');
+            disp(b);
+            for i=1:itmax
+                eit = 0;
+                t = 0;
+                for j=1:numProt
+                    a = hardlim(W*prototipos(j,:)'+b);
+                    ej = targets(j,:)-a;
+                    if ej ~= 0
+                        t = t+1;
+                        W = W+ej*prototipos(j,:);
+                        b = b+ej;
+                        eit = eit+(1/numProt)*ej;
+                    end
+                end
+                if eit <= Eit && t == 0
+                    fprintf('Se tuvo un aprendizaje exitoso en la iteraci贸n %d.\n',i);
+                    break;
+                end
+            end
+            disp('W =');
+            disp(W);
+            disp('b =');
+            disp(b);
+        end
+    otherwise
+        display('Opci贸n inv谩lida');
 end
