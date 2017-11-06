f7d55005b4fda43d0325c852a4ee57783ba71c26
Ivan Ortega Victoriano <iortegav1500@alumno.ipn.mx>
2017-11-05 18:22:03 -0600

    OK

--- Practica-3/Perceptron-Simple/leerPatronesPrototipo.m
+++ Practica-3/Perceptron-Simple/leerPatronesPrototipo.m
@@ -1,18 +1,20 @@
+% Esta función lee los valores de los vectores prototipo p de un archivo 
+% .txt del conjunto de entrenamiento que se encuentren en un formato:
+% {[p1 p2 p3 ... pn],[t1 t2 t3 ... tn]}
 function A = leerPatronesPrototipo(nombre,targetDim,numProt,protDim)
     archivo = fopen(nombre,'r');
     A = zeros(numProt,protDim);
+    disp([numProt protDim]);
+    while archivo == -1
+        nombre = input('Ingrese un nombre válido:  ','s');
+        archivo = fopen(nombre,'r');
+    end
     for i=1:numProt
-        A(i,1) = fscanf(archivo,'{[%d');
-        for j=1:protDim-2
-            A(i,j) = fscanf(archivo,' %d');
-        end
-        A(i,protDim) = fscanf(archivo,' %d],');
-        fscanf(archivo,'[%d');
-        for j=1:targetDim-2
-            fscanf(archivo,' %d');
-        end
-        fscanf(archivo,' %d]},');
-        fscanf(archivo,'\n');
+        fscanf(archivo,'{[');
+        A(i,:) = fscanf(archivo,'%d');
+        fscanf(archivo,'],[');
+        fscanf(archivo,'%d');
+        fscanf(archivo,']}\n');
     end
     fclose(archivo);
 end
