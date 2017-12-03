5b1826bbc1ec0864cd7c3d0fd8ebc76e24cb0f53
Ivan Ortega Victoriano <iortegav1500@alumno.ipn.mx>
2017-12-02 22:49:06 -0600

    Corrección de error
    No se llenaba completamente la matriz del conjunto de validación. Se solucionó el error.

--- MLP/obtenerConjuntoDeValidacion.m
+++ MLP/obtenerConjuntoDeValidacion.m
@@ -1,6 +1,6 @@
 function M = obtenerConjuntoDeValidacion(entradas,targets,num_datos,num_elem)
 M = zeros(num_elem,2);
-inc = round(num_datos/(num_elem+1));
+inc = ceil(num_datos/(num_elem+1));
 j = 1;
 for i=inc:inc:num_datos
     M(j,1) = entradas(i);
@@ -8,6 +8,16 @@ for i=inc:inc:num_datos
     j = j+1;
     if j == num_elem+1
         break;
+    end
+end
+if j ~= num_elem+1
+    for i=inc+1:inc:num_datos
+        M(j,1) = entradas(i-1);
+        M(j,2) = targets(i-1);
+        j = j+1;
+        if j == num_elem+1
+            break;
+        end
     end
 end
 end
