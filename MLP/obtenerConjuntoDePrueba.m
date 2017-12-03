166ccc8b523178e4f0defaa5506233d208714c7f
Ivanovsky <ivanovskyortega@gmail.com>
2017-12-02 20:43:47 -0600

    Funcion que obtiene el conjunto de prueba

---
+++ MLP/obtenerConjuntoDePrueba.m
@@ -0,0 +1,13 @@
+function M = obtenerConjuntoDePrueba(entradas,targets,num_datos,num_elem)
+M = zeros(num_elem,2);
+inc = round(num_datos/(num_elem+1));
+j = 1;
+for i=inc:inc:num_datos
+    M(j,1) = entradas(i-1);
+    M(j,2) = targets(i-1);
+    j = j+1;
+    if j == num_elem+1
+        break;
+    end
+end
+end
