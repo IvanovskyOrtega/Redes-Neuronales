0c5f05172147274d7182725d39518c5f3dc8283c
Ivan Ortega Victoriano <iortegav1500@alumno.ipn.mx>
2017-12-03 10:42:35 -0600

    Se ahorran lineas de código

--- MLP/obtenerF.m
+++ MLP/obtenerF.m
@@ -1,16 +1,10 @@
 function F = obtenerF(tipo_funcion,num_neuronas,a)
     switch tipo_funcion
         case 1
-            derivada = ones(1,num_neuronas);
-            F = diag(derivada);
+            F = diag(ones(1,num_neuronas));
         case 2
-            unos = ones(1,num_neuronas);
-            derivada = diag(unos);
-            derivada = derivada - diag(a');
-            F = derivada*diag(a');
+            F = diag(logsig('dn',a,a));
         case 3
-            unos = ones(1,num_neuronas);
-            derivada = diag(unos);
-            F = derivada - (diag(a')*diag(a'));
+            F = diag(tansig('dn',a,a));
     end
 end
